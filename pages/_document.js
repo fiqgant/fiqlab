5bdb9668d9ccab11526813998df39f1d9ff6f8f9
fiqgant <fiqgant@gmail.com>
2022-08-03 13:39:29 +0700

    🔎 change meta description

--- pages/_document.js
+++ pages/_document.js
@@ -100,7 +100,7 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <meta
             name="description"
-            content="Welcome to my personal blog where I share my musings. I am a Data Engineer passionate about Data Science, building pipelines and Automation."
+            content="Welcome to my personal blog where I share my work, article, etc. I am a geek who is passionate about Machine Learning and Artificial Intelligence."
           />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
