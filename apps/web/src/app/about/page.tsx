6ecdda6a96c5efb6697df405ded9e45cfa52cbc3
Taufiqurrahman <fiqgant@gmail.com>
2024-07-06 23:38:40 +0700

    change all

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -6,7 +6,7 @@ import Mdx from '@/components/mdx'
 import PageTitle from '@/components/page-title'
 
 const title = 'About'
-const description = '👋 Hi there! I am Hong, a student who loves web development.'
+const description = '👋 Hi there! I am fiq, a person love to learn.'
 
 type PageProps = {
   params: Record<string, never>
