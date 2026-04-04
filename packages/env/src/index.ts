231b40cb2e022858960fe9dfe2adb89288455645
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2026-04-05 00:27:42 +0700

    fix: remove GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET from env validation

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -35,8 +35,6 @@ export const env = createEnv({
     ...(flags.auth
       ? {
           NEXTAUTH_SECRET: z.string().min(1),
-          GOOGLE_CLIENT_ID: z.string().min(1),
-          GOOGLE_CLIENT_SECRET: z.string().min(1),
           GITHUB_CLIENT_ID: z.string().min(1),
           GITHUB_CLIENT_SECRET: z.string().min(1)
         }
