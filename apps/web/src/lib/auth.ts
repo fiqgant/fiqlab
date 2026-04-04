962fa6713d6e530fd4be1a7ab3be234179449202
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2026-04-05 00:26:35 +0700

    Remove Google provider and keep only GitHub provider in auth.ts

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -5,7 +5,6 @@ import { env } from '@tszhong0411/env'
 import type { DefaultSession, NextAuthConfig } from 'next-auth'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
 import { cache } from 'react'
 
 import { getDefaultUser } from '@/utils/get-default-user'
@@ -32,10 +31,6 @@ const config: NextAuthConfig = {
     GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET
-    }),
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET
     })
   ],
 
@@ -48,19 +43,6 @@ const config: NextAuthConfig = {
 
   callbacks: {
     async signIn({ account, profile, user }) {
-      // Update data when user signs in every time
-      if (account?.provider === 'google') {
-        if (!profile) return true
-
-        await db
-          .update(users)
-          .set({
-            name: profile.name,
-            image: profile.picture
-          })
-          .where(eq(users.id, user.id!))
-      }
-
       if (account?.provider === 'github') {
         if (!profile) return true
 
